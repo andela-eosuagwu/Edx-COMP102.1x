af33ea367ba082db2d09e05766b581f6dce2fbaa
Emeka Osuagwu <emekaosuagwuandela@gmail.com>
2015-10-23 18:22:24 +0100

    Push Car Intro

---
+++ Car Intro/Car.java
@@ -0,0 +1,29 @@
+import comp102x.IO;
+
+/**
+ * A class of Car objects that can move forward, backward and turn
+ */
+public class Car
+{
+    
+    int odometer;
+    String owner = "No name";
+    
+    public Car(){}
+    
+    public void getName (String name)
+    {
+        owner = name;
+    }
+    
+    
+    public void moveCar (int dist)
+    {
+        odometer = odometer + Math.abs(dist);
+    }
+
+   public void getOdometer ()
+   {
+       IO.output(odometer);
+   }
+}
